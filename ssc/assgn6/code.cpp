69b412470edee1cc69ee400391a01c9c5a9ea6cf
hrishikesh214 <hrishikesh.vaze01@gmail.com>
2022-10-13 23:43:29 +0530

    final sync all

--- ssc/assgn6/code.cpp
+++ ssc/assgn6/code.cpp
@@ -134,7 +134,7 @@ bool F()
 
 int main()
 {
-    inStr = "(a + a  a)";
+    inStr = "a + ( s + i )";
 
     cout << endl
          << "Enter string without space: ";
@@ -159,85 +159,3 @@ int main()
     return 0;
 }
 // s = a + i
-
-/*
-
-  #include<stdio.h>
-  #include<conio.h>
-  #include<stdlib.h>
-
-  void E();
-  int i=0;
-  char str[10], tp;
-
-  void advance() {
-      i++;
-      tp=str[i];
-  }
-
-  void F() {
-      if(tp=='i') {
-          advance();
-      }
-      else {
-          if(tp=='(') {
-              advance();
-              E();
-              if(tp==')') {
-                  advance();
-              }
-          }
-          else {
-              printf("\nString not Accepted");
-              exit(1);
-          }
-      }
-  }
-
-  void TP() {
-      if(tp=='*') {
-          advance();
-          F();
-          TP();
-      }
-  }
-
-  void T() {
-      F();
-      TP();
-  }
-
-  void EP() {
-      if(tp=='+') {
-          advance();
-          T();
-          EP();
-      }
-  }
-
-  void E() {
-      T();
-      EP();
-  }
-
-  int main() {
-      int op;
-      while(1) {
-          printf("\nEnter the string: ");
-          scanf("%s", &str);
-          tp=str[i];
-          E();
-          if(tp=='\0')
-              printf("\nString Accepted");
-          else
-              printf("\nString not Accepted");
-
-          printf("\nEnter 1 for exit");
-          scanf("%d", &op);
-          if(op==1)
-              exit(0);
-      }
-  }
-
-
-*/
